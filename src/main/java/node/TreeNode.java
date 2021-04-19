adb68820be72098f7235f62cabd857a7c6f46cda
ruiqi.he <ruiqi.he@yaduo.com>
2021-04-19 15:20:31 +0800

    commit

--- src/main/java/node/TreeNode.java
+++ src/main/java/node/TreeNode.java
@@ -1,13 +1,31 @@
 package node;
 
-public class TreeNode {
-   public int val;
-   public TreeNode left;
-   public TreeNode right;
+import java.util.StringJoiner;
 
-   public TreeNode(int x) {
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int x) {
         val = x;
     }
 
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
 
+    @Override
+    public String toString() {
+        StringJoiner joiner = new StringJoiner(",","[","]");
+
+        TreeNode  left=this;
+        while (left!=null){
+            joiner.add(String.valueOf(left.val));
+            left=left.right;
+        }
+        return joiner.toString();
+    }
 }
